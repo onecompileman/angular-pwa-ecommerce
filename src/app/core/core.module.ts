d15a8646cf90e7a03e3bee9cfb25d04058fe281f
Stephen Vinuya <stephen.vinuya@ffuf.de>
2019-03-08 18:11:26 +0800

    setup services

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,17 +3,25 @@ import { CommonModule } from '@angular/common';
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SharedModule } from '../shared/shared.module';
+import * as fromServices from './services';
+import { environment } from '../../environments/environment';
+import { RestangularModule } from 'ngx-restangular';
 
 @NgModule({
   declarations: [NavigationComponent],
-  exports: [NavigationComponent],
+  exports: [NavigationComponent, ...fromServices.services],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RestangularModule.forRoot(RestangularConfigFactory)
   ]
 })
-export class CoreModule { 
+export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
   }
+}
+
+export function RestangularConfigFactory(RestangularProvider) {
+  RestangularProvider.setBaseUrl(environment.api);
 }
